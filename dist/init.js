f8af6403ecda5e473dcb9f38d4c0c6881c7cabb2
luoyang <luoyang@mogujie.com>
2020-03-27 10:54:31 +0800

    增加远端项目创建

--- dist/init.js
+++ dist/init.js
@@ -12,25 +12,51 @@ var _chalk = require('chalk');
 
 var _chalk2 = _interopRequireDefault(_chalk);
 
+var _ora = require('ora');
+
+var _ora2 = _interopRequireDefault(_ora);
+
+var _util = require('./util.js');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-let init = () => {
-    _child_process2.default.exec(`git init`, () => {
-        console.log(_logSymbols2.default.success, _chalk2.default.green('git 初始化完成'));
-        _child_process2.default.exec(`npm install`, (error, stdout, stderr) => {
-            console.log(_logSymbols2.default.success, _chalk2.default.green(stdout));
-            console.log(_logSymbols2.default.error, _chalk2.default.red(stderr));
-            if (error !== null) {
-                console.log(_logSymbols2.default.error, _chalk2.default.red(error));
-            } else {
-                console.log(_logSymbols2.default.success, _chalk2.default.green('npm依赖安装完成'));
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+
+
+let init = async () => {
+
+    try {
+        await loadCmd(`git init`, 'git初始化');
+        const projectName = process.cwd().split('/').slice(-1)[0];
+        await loadCmd(`curl -u "LuoYangYY:34f880e58434244e3dfca70ef646eeb27ad0b0e9" https://api.github.com/user/repos -d '{"name": "${projectName}"}'`, 'Github仓库创建');
+        await loadCmd(`git remote add origin https://github.com/LuoYangYY/${projectName}.git`, '关联远端仓库');
+        let loading = (0, _ora2.default)();
+        loading.start(`package.json更新repository: 命令执行中...`);
+        await (0, _util.updateJsonFile)('package.json', {
+            "repository": {
+                "type": "git",
+                "url": `https://github.com/LuoYangYY/${projectName}.git`
             }
-        }, err => {
-            console.log(err);
+        }).then(() => {
+            loading.succeed(`package.json更新repository: 命令执行完成`);
         });
-    }, err => {
-        console.log(err);
-    });
+        await loadCmd(`git add .`, '执行git add');
+        await loadCmd(`git commit -a -m 'init'`, '执行git commit');
+        await loadCmd(`git push --set-upstream origin master`, '执行git push');
+        await loadCmd(`npm install`, '安装依赖');
+    } catch (err) {
+        console.log(_logSymbols2.default.error, _chalk2.default.red('初始化失败'));
+        console.log(_logSymbols2.default.error, _chalk2.default.red(err));
+        process.exit(1);
+    }
+};
+
+let loadCmd = async (cmd, text) => {
+    let loading = (0, _ora2.default)();
+    loading.start(`${text}: 命令执行中...`);
+    await exec(cmd);
+    loading.succeed(`${text}: 命令执行完成`);
 };
 
 module.exports = init;
